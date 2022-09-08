1a3df14e1b377ef54426cb869f4c338ffcb5c865
MtbEvolution <107712954+MtbEvolution@users.noreply.github.com>
2022-09-08 15:57:25 -0400

    Add files via upload

---
+++ dNdS/pNpS.sh
@@ -0,0 +1,4 @@
+# Usage
+# Adapted from A. Trauner, Q. Liu et al., The within-host population dynamics of Mycobacterium tuberculosis vary with treatment efficacy. Genome biology 18, 71 (2017).
+# Prepare "Gene.Mutation.ann.csv" file as the format showed in the template.
+python3 pNS_setsynto1.py Gene.Mutation.ann.csv   
